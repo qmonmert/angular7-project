063bd24bf96eb9f9036ebcc439fb883558b6ea37
Quentin Monmert <qmonmert@qmonmert.home>
2018-10-21 20:40:39 +0200

    Toolbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
+import { HomeComponent } from "./home/home.component";
 import { VirtualScrollComponent } from "./virtual-scroll/virtual-scroll.component";
 import { DragDropComponent } from "./drag-drop/drag-drop.component";
 import { DragDropAdvancedComponent } from "./drag-drop-advanced/drag-drop-advanced.component";
 
 const routes: Routes = [
-  { path: "", component: VirtualScrollComponent },
+  { path: "", component: HomeComponent },
+  { path: "virtual-scroll", component: VirtualScrollComponent },
   { path: "drag-drop", component: DragDropComponent },
   { path: "drag-drop-advanced", component: DragDropAdvancedComponent },
-  { path: "**", component: VirtualScrollComponent }
+  { path: "**", component: HomeComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
+import { MatTabsModule } from "@angular/material/tabs";
+
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
@@ -8,12 +10,20 @@ import { ScrollingModule } from "@angular/cdk/scrolling";
 import { DragDropModule } from "@angular/cdk/drag-drop";
 import { VirtualScrollComponent } from "./virtual-scroll/virtual-scroll.component";
 import { DragDropComponent } from "./drag-drop/drag-drop.component";
-import { DragDropAdvancedComponent } from './drag-drop-advanced/drag-drop-advanced.component';
+import { DragDropAdvancedComponent } from "./drag-drop-advanced/drag-drop-advanced.component";
+import { HomeComponent } from "./home/home.component";
 
 @NgModule({
-  declarations: [AppComponent, VirtualScrollComponent, DragDropComponent, DragDropAdvancedComponent],
+  declarations: [
+    AppComponent,
+    VirtualScrollComponent,
+    DragDropComponent,
+    DragDropAdvancedComponent,
+    HomeComponent
+  ],
   imports: [
     BrowserModule,
+    MatTabsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     ScrollingModule,
